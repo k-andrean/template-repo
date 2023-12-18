88a2fd9e61be1e89c4943896ec2971d4b37acf8e
kelvin <baganceshop@gmail.com>
2023-12-18 17:42:41 +0700

    adding babel loader config and install instruction

--- webpack.config.js
+++ webpack.config.js
@@ -18,10 +18,22 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
-     {
-       test: /\.(png|svg|jpg|jpeg|gif)$/i,
-       type: 'asset/resource',
-     },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+            ],
+          },
+        },
+      },
     ],
   },
   plugins: [
